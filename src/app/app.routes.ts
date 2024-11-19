12e273afbb619b1bf6b5aa70058f6a599040b0c8
HAROLD MUÑOZ <36018526+harold1978@users.noreply.github.com>
2024-11-19 15:51:06 -0600

    router actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 import { Component } from '@angular/core';
 
 import { InicioComponent } from './paginas/inicio/inicio.component';
-import { PollosComponent } from './paginas/pollos/pollos.component';
+
 export const routes: Routes = [
    
     {path:'',component:InicioComponent},
